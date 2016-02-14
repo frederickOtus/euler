e1701375cb6301004ed66222581aec8918b3fa76
peter <pita.da.bread07@gmail.com>
2016-02-13 22:02:39 -0500

    did some P61 refactoring to make it less bad, not solved yet though

--- haskell/P61.hs
+++ haskell/P61.hs
@@ -1,30 +1,19 @@
 import Data.List
 
-tri = [ truncate val | n <- [1 .. 141], let val = n*(n+1)/2, val > 999 && val < 10000 ]
-sqr = [ truncate val | n <- [1 .. 141], let val = n*n, val > 999 && val < 10000 ]
-pen = [ truncate val | n <- [1 .. 141], let val = n*(3*n-1)/2, val > 999 && val < 10000 ]
-hex = [ truncate val | n <- [1 .. 141], let val = n*(2*n-1), val > 999 && val < 10000 ]
-hep = [ truncate val | n <- [1 .. 141], let val = n*(5*n-3)/2, val > 999 && val < 10000 ]
-oct = [ truncate val | n <- [1 .. 141], let val = n*(3*n-2), val > 999 && val < 10000 ]
+numOps [] = []
+numOps (op:ops) = ns : numOps ops
+    where ns = takeWhile (<10000) $ filter (> 999) $ map (truncate . op) [ 1 .. ]
 
-nums = [ [t,s,p,h,hp,o] | t <- tri, s <- sqr, p <-pen, h <-hex, hp <- hep, o <-oct ]
+nums = numOps [ (\n -> n*(n+1)/2), (\n -> n*n), (\n -> n*(3*n-1)/2), (\n -> n*(2*n-1)), (\n -> n*(5*n-3)/2), (\n -> n*(3*n-2)) ]
 
-isCycle :: [Int] -> Bool
-isCycle l@(x:xs) = tCycle (l ++ [x])
+buildCycle n [] = [n]
+--buildCycle n (x) =
 
-tCycle :: [Int] -> Bool
-tCycle [] = True
-tCycle [n] = True
-tCycle (n1:n2:ns) = (a == b) && (tCycle (n2:ns))
-    where a = mod n1 100
-          b = quot n2 100
+pPairs n ns = filter t ns
+    where t = \i -> (quot i 100) == (mod n 100)
 
-testP [] = False
-testP (x:xs) = (isCycle x) || (testP xs)
 
-test n = testP $ permutations n
+--solve = find testP nums
 
-solve = find testP nums
-
-main :: IO()
-main = print $ show $ solve
+--main :: IO()
+--main = print $ show $ solve
