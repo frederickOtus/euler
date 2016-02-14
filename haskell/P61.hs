6a77b014db16441fb1f9a3c3c570802ac7dc9209
peter <pita.da.bread07@gmail.com>
2016-02-14 15:18:19 -0500

    finished P61 for hs

--- haskell/P61.hs
+++ haskell/P61.hs
@@ -6,14 +6,26 @@ numOps (op:ops) = ns : numOps ops
 
 nums = numOps [ (\n -> n*(n+1)/2), (\n -> n*n), (\n -> n*(3*n-1)/2), (\n -> n*(2*n-1)), (\n -> n*(5*n-3)/2), (\n -> n*(3*n-2)) ]
 
-buildCycle n [] = [n]
---buildCycle n (x) =
+
+initPrefs = map (\n -> [n]) (nums !! 0)
+initSilos = drop 1 nums
+
+siloPairings prefs silo = foldl (++) [] $ map spairs prefs
+    where spairs pref = [ s:pref | s <- pPairs (head pref) silo ]
 
 pPairs n ns = filter t ns
-    where t = \i -> (quot i 100) == (mod n 100)
+    where t = \i -> (quot i 100) == (mod n 100) --4 digit numbers, the last 2 digs of first are first 2 of second
 
+allbut = \l n -> (take n l) ++ (drop (n + 1) l)
 
---solve = find testP nums
+proc p [] = p
+proc p s = foldl (++) [] (map nextIter sets)
+    where sets = [ (s !! n, allbut s n) | n <- [ 0 .. (length s) - 1] ]
+          nextIter (st,sts) = proc (siloPairings p st) sts
 
---main :: IO()
---main = print $ show $ solve
+solve = sum $ nums !! 0
+    where f = \l -> (mod (head l) 100) == (quot (last l) 100)
+          nums = filter f $ proc initPrefs initSilos
+
+main :: IO()
+main = print $ show $ solve
