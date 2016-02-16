ce7daac172f494ff6b5e9fe5b747ca706cc31f4a
peter <pita.da.bread07@gmail.com>
2016-02-15 21:43:46 -0500

    sol for p63 haskell

---
+++ haskell/P63.hs
@@ -0,0 +1,11 @@
+import Data.List 
+
+solve = length sols
+    where sols = foldl (++) [] $ takeWhile ((>0) . length) $ map nDigs [1..]
+
+nDigs n = takeWhile (<mx) $ filter (>mn) seq
+    where seq = map (^n) [1..]
+          mx = 10 ^ n 
+          mn = 10 ^ (n - 1) - 1
+
+main = print $ show $ solve
