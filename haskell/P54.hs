0c6de318367a1f08c7192a748117441b6d23d293
peter <pita.da.bread07@gmail.com>
2016-03-02 23:25:37 -0500

    halfway to pair matching

--- haskell/P54.hs
+++ haskell/P54.hs
@@ -1,3 +1,5 @@
+import Data.List
+
 type Card = (Int, Int) --first int is val, second is suit
 
 sameSuit ((c,s):hand) = all (\(_,s')-> s' == s) hand
@@ -12,4 +14,17 @@ rank hand@((c,s):h)
     | isSeq hand && sameSuit hand = (9, handMax hand, handMax hand)
     | sameSuit hand = (5, handMax hand, handMax hand)
     | isSeq hand = (4, handMax hand, handMax hand)
-    | otherwise = (0, handMax hand, handMax hand)
+    | length ms == length hand = (0, handMax hand, handMax hand)
+    where ms = smatches hand
+
+smatches :: [Card] -> [(Int, Int)]
+smatches hand = sortBy (flip cmp) ms
+    where ms = matches hand
+          cmp (ps,vls) (ps',vls') = if ps == ps' then compare vls vls' else compare ps ps'
+
+matches :: [Card] -> [(Int, Int)] --size / card
+matches [] = []
+matches all@((c,s):cs) = (cnt,c) : matches rest
+    where cnt = length $ takeWhile (\(c',_) -> c == c') all
+          rest = dropWhile (\(c',_) -> c == c') all
+
